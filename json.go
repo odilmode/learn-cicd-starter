19c3efdc63f447c177945e8c9255d0c20704736a
username <you@example.com>
2025-07-23 17:43:10 +0900

    Made some fixes

--- json.go
+++ json.go
@@ -30,7 +30,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	_, err := w.Write(dat) {
+	_, err := w.Write(dat) 
+	if err != nil {
 		log.Printf("Error writing JSON: %s", err)
 		return
 	}
